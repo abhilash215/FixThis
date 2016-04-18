093f71da4af9848eb495df7048d344646f1ad7d7
bschiranth@gmail.com <fordGT500@>
2016-04-18 14:20:48 -0400

    - added login activity - added some libraries to gradle files

--- app/src/main/java/com/example/abhiu/myapplication/Activities/LoginActivity.java
+++ app/src/main/java/com/example/abhiu/myapplication/Activities/LoginActivity.java
@@ -9,6 +9,180 @@ import android.view.View;
 
 import com.example.abhiu.myapplication.R;
 
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.design.widget.Snackbar;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.ui.auth.core.AuthProviderType;
+import com.firebase.ui.auth.core.FirebaseLoginBaseActivity;
+import com.firebase.ui.auth.core.FirebaseLoginError;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import java.util.Map;
+
+public class LoginActivity extends FirebaseLoginBaseActivity{
+
+    Firebase firebaseRef;
+    EditText userNameET;
+    EditText passwordET;
+    String mName;
+
+    /* String Constants */
+    private static final String FIREBASEREF = "https://fixthis.firebaseio.com/";
+    private static final String FIREBASE_ERROR = "Firebase Error";
+    private static final String USER_ERROR = "User Error";
+    private static final String LOGIN_SUCCESS = "Login Success";
+    private static final String USER_CREATION_SUCCESS =  "Successfully created user";
+    private static final String USER_CREATION_ERROR =  "User creation error";
+    private static final String EMAIL_INVALID =  "email is invalid :";
+    private static final String LOGGED_OUT =  "Logged out :";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ////////////////////////////////
+        Firebase.setAndroidContext(this);
+        //////////////////////////////////////////
+        firebaseRef = new Firebase(FIREBASEREF);
+        super.onCreate(savedInstanceState);
+        ////////////////////////////////////////////////////////////////////////////////
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.abhiu.myapplication.Activities",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(getApplicationContext(),"Package name error",Toast.LENGTH_SHORT);
+
+        } catch (NoSuchAlgorithmException e) {
+            Toast.makeText(getApplicationContext(),"No algorith found",Toast.LENGTH_SHORT);
+        }
+        ////////////////////////////////////////////////////////////////////////////////
+        setContentView(R.layout.activity_login);
+
+        userNameET = (EditText)findViewById(R.id.edit_text_email);
+        passwordET = (EditText)findViewById(R.id.edit_text_password);
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.showFirebaseLoginPrompt();
+            }
+        });
+
+        Button createButton = (Button) findViewById(R.id.button);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onFirebaseLoginProviderError(FirebaseLoginError firebaseLoginError) {
+        Snackbar snackbar = Snackbar.
+                make(userNameET, FIREBASE_ERROR + firebaseLoginError.message, Snackbar.LENGTH_SHORT);
+        snackbar.show();
+        resetFirebaseLoginPrompt();
+    }
+
+    @Override
+    protected void onFirebaseLoginUserError(FirebaseLoginError firebaseLoginError) {
+        Snackbar snackbar = Snackbar
+                .make(userNameET, USER_ERROR + firebaseLoginError.message, Snackbar.LENGTH_SHORT);
+        snackbar.show();
+        resetFirebaseLoginPrompt();
+    }
+
+    @Override
+    public Firebase getFirebaseRef() {
+        return firebaseRef;
+    }
+
+    @Override
+    public void onFirebaseLoggedIn(AuthData authData) {
+        switch (authData.getProvider()) {
+            case "password":
+                mName = (String) authData.getProviderData().get("email");
+                break;
+            default:
+                mName = (String) authData.getProviderData().get("displayName");
+                break;
+        }
+        Toast.makeText(getApplicationContext(), LOGIN_SUCCESS, Toast.LENGTH_SHORT).show();
+        Intent myIntent = new Intent(LoginActivity.this, MainActivity.class);
+        LoginActivity.this.startActivity(myIntent);
+    }
+
+    @Override
+    public void onFirebaseLoggedOut() {
+        //2Toast.makeText(getApplicationContext(), LOGIN_SUCCESS, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // All providers are optional! Remove any you don't want.
+        setEnabledAuthProvider(AuthProviderType.PASSWORD);
+        setEnabledAuthProvider(AuthProviderType.GOOGLE);
+//      setEnabledAuthProvider(AuthProviderType.FACEBOOK);
+        // setEnabledAuthProvider(AuthProviderType.TWITTER);
+    }
+
+    // Validate email address for new accounts.
+    private boolean isEmailValid(String email) {
+        boolean isGoodEmail = (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            userNameET.setError(EMAIL_INVALID + email);
+            return false;
+        }
+        return true;
+    }
+
+    // create a new user in Firebase
+    public void createUser() {
+        if(userNameET.getText() == null ||  !isEmailValid(userNameET.getText().toString())) {
+            return;
+        }
+        firebaseRef.createUser(userNameET.getText().toString(), passwordET.getText().toString(),
+                new Firebase.ValueResultHandler<Map<String, Object>>() {
+                    @Override
+                    public void onSuccess(Map<String, Object> result) {
+                        Snackbar snackbar = Snackbar.make(userNameET, USER_CREATION_SUCCESS, Snackbar.LENGTH_SHORT);
+                        snackbar.show();
+                    }
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+                        Snackbar snackbar = Snackbar.make(userNameET, USER_CREATION_ERROR, Snackbar.LENGTH_SHORT);
+                        snackbar.show();
+                    }
+                });
+    }
+}
+
+/*
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -18,14 +192,8 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
+*/
