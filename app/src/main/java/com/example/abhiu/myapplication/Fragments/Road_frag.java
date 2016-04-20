3f1beb31ba4e4f12ebb28fc52db43e2d07278af5
bschiranth@gmail.com <fordGT500@>
2016-04-19 22:32:42 -0400

    -camera and gallery

--- app/src/main/java/com/example/abhiu/myapplication/Fragments/Road_frag.java
+++ app/src/main/java/com/example/abhiu/myapplication/Fragments/Road_frag.java
@@ -1,29 +1,107 @@
 package com.example.abhiu.myapplication.Fragments;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.abhiu.myapplication.Activities.MainActivity;
 import com.example.abhiu.myapplication.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class Road_frag extends Fragment {
+    ImageView iv;
+
+    private static final int REQUEST_CAMERA = 123, SELECT_FILE=1; // integer request code for camera
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    private void selectImage() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library", "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                } else if (items[item].equals("Choose from Library")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"),
+                            SELECT_FILE);
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
     public Road_frag()
     {
 
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+      /*  if(requestCode==CAMERA_REQUEST && resultCode== getActivity().RESULT_OK)
+        {
+            Bitmap bitmap=(Bitmap)data.getExtras().get("data");
+            iv.setImageBitmap(bitmap);
+            Toast.makeText(getContext(), "Image Saved in Gallery", Toast.LENGTH_SHORT).show();
+        }*/
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                           Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_road, container, false);
+       View rootView =  inflater.inflate(R.layout.fragment_road, container, false);
+
+        iv =(ImageView) rootView.findViewById(R.id.camera_road);
+       iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//start new activity for image capture
+                //getActivity().startActivityForResult(i, CAMERA_REQUEST);
+                selectImage();
+            }
+        });
 
 
-
-
+        return rootView;
     }
 
     private static final String ARG_SECTION_NUMBER="section_number";
@@ -36,6 +114,56 @@ public class Road_frag extends Fragment {
         return  fragment;
     }
 
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
 
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        iv.setImageBitmap(thumbnail);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+        Uri selectedImageUri = data.getData();
+        String[] projection = { MediaStore.MediaColumns.DATA };
+        Cursor cursor = getActivity().managedQuery(selectedImageUri, projection, null, null,
+                null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+
+        String selectedImagePath = cursor.getString(column_index);
+
+        Bitmap bm;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(selectedImagePath, options);
+        final int REQUIRED_SIZE = 200;
+        int scale = 1;
+        while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+            scale *= 2;
+        options.inSampleSize = scale;
+        options.inJustDecodeBounds = false;
+        bm = BitmapFactory.decodeFile(selectedImagePath, options);
+
+        iv.setImageBitmap(bm);
+    }
+    /////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
